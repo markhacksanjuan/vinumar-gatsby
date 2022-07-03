628d1e0f4f051446ae381d93012ddea62556f305
Mark <mark.sanjua.sole@gmail.com>
2022-07-04 00:38:13 +0200

    chore added animations

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,19 +39,22 @@ const IndexPage = ({}) => {
         <div className='index-container-header'>
           <img src={headerImg} alt='Vinumar' />
           <div className="index-container-header-h2">
-            <h2>Más de 60 años de tradición e innovación en el mundo de la uva.</h2>
+            <h2 className="index-h2">Más de 60 años de tradición e innovación en el mundo de la uva.</h2>
           </div>
-          <Button className='index-card-button' style='white-button' goTo='#productos'>VER PRODUCTOS</Button>
+          {/* <Button className='index-card-button button-fade-in' style='white-button' goTo='#productos'>VER PRODUCTOS</Button> */}
+          <div className="button-fade-in">
+            <Button style='white-button' goTo='#productos'>VER PRODUCTOS</Button>
+          </div>
 
         </div>
       </>
     )
   }
-  const ProductoCard = ({ title, text, image, goTo, cardClass, scrollCard }) => {
+  const ProductoCard = ({ title, text, image, goTo }) => {
     return(
       <>
         {/* <div className={`index-card`}> */}
-          <div className='index-container-text-card'>
+          <div className={`index-container-text-card`}>
             <p className='index-title-card'>{title}</p>
             <p className='index-text-card'>{text}</p>
           </div>
@@ -71,18 +74,15 @@ const IndexPage = ({}) => {
         <div id='productos' className={`index-container-product ${scroll > 800 && 'index-container-product-animated'}`}>
           <h2>Productos</h2>
           <div className='index-container-cards'>
-            <div className={`index-card ${scroll > 800 && 'index-card-animated-bottom'}`}>
+            <div className={`index-card ${scroll > 1000 && 'index-card-animated-bottom'}`}>
               <ProductoCard 
                 title='Extractos de piel de uva'
                 text= 'Es uno de los colorantes naturales más destacados en el mundo de la alimentación. El extracto de piel de uva permite una amplia variedad de usos en todos sus tonos de color.'
                 image={extPiel}
                 goTo='/productos/ExtractosPiel'
-                // className={scroll > 600 && 'index-card-animated'}
-                // scrollCard={600}
-                // cardClass='index-card-animated-left'
               />
             </div>
-            <div className={`index-card ${scroll > 900 && 'index-card-animated-bottom'}`}>
+            <div className={`index-card ${scroll > 1200 && 'index-card-animated-bottom'}`}>
               <ProductoCard 
                 title='Extractos de vino'
                 text= 'Aromatizante y saborizante concentrado y desalcoholizado elaborado a partir de vinos tintos y blancos seleccionados. El extracto de vino es el ingrediente secreto de salsas, platos preparados, bebidas no alcohólicas, etc. de uso muy frecuente en alimentación.'
@@ -90,7 +90,7 @@ const IndexPage = ({}) => {
                 goTo='/productos/ExtractosVino'
               />
             </div>
-            <div className={`index-card ${scroll > 1100 && 'index-card-animated-bottom'}`}>
+            <div className={`index-card ${scroll > 1400 && 'index-card-animated-bottom'}`}>
               <ProductoCard 
                 title='Taninos de uva'
                 text= 'Son compuestos fenólicos naturales que se extraen de la piel y las pepitas de la uva. Los taninos, además de potenciadores del sabor, aportan múltiples cualidades en el campo de la enología.'
@@ -98,7 +98,7 @@ const IndexPage = ({}) => {
                 goTo='/productos/Taninos'
               />
             </div>
-            <div className={`index-card ${scroll > 1250 && 'index-card-animated-bottom'}`}>
+            <div className={`index-card ${scroll > 1600 && 'index-card-animated-bottom'}`}>
               <ProductoCard 
                 title='Alcoholes'
                 text= 'Fabricados a partir del vino o los subproductos de la vinificación, los alcoholes abarcan desde los aguardientes a los utilizados por la industria química y farmacéutica, el sector sanitario o hasta el energético.'
@@ -106,7 +106,7 @@ const IndexPage = ({}) => {
                 goTo='/productos/Alcoholes'
               />
             </div>
-            <div className={`index-card ${scroll > 1500 && 'index-card-animated-bottom'}`}>
+            <div className={`index-card ${scroll > 1800 && 'index-card-animated-bottom'}`}>
               <ProductoCard 
                 title='Pepita de uva'
                 text= 'Fruto del secado del orujo de la uva y su posterior separación, la pepita de uva es la base del aceite de granilla, que contiene vitamina E y ácidos grasos esenciales omega 3 y omega 6.'
@@ -123,21 +123,21 @@ const IndexPage = ({}) => {
   const renderDonde = () => {
     return(
       <>
-        <div className={`index-container-donde ${scroll > 2300 && 'index-container-donde-animated'}`}>
+        <div className={`index-container-donde ${scroll > 2400 && 'donde-animated'}`}>
           <h2>Dónde estamos</h2>
-          <div className='index-card-donde'>
-            <div className='index-container-donde-text'>
-              <p className='index-donde-direccion'>ctra. Munera, 5
+          <div className={`index-card-donde `}>
+            <div className={`index-container-donde-text `}>
+              <p className={`index-donde-direccion ${scroll > 2400 && 'index-donde-direccion-animated'}`}>ctra. Munera, 5
                 <span>02600 Villarrobledo</span>
                 <span>Albacete, España</span>
               </p>
-              <div>
+              <div className={`index-donde-text ${scroll > 2400 && 'index-donde-text-animated'}`}>
                 <p>Nos ubicamos en una localización estratégica: Castilla-La Mancha, una de las mayores regiones productoras de uva de alta calidad a escala mundial. Esta cercanía a la materia prima nos garantiza abundancia de suministro y una gran capacidad de producción, para responder a las demandas de nuestros clientes con la máxima rapidez.
                 <span className="parrafo">Además, contamos con una red logística de largo alcance por autovía, ferrocarril, transporte aéreo y marítimo, que nos permite llegar allí donde sea preciso. Desde donde estamos todo son ventajas.</span>
                 </p>
               </div>
             </div>
-              <img className='index-donde-img' src={dondeImg} alt='Donde estamos' />
+              <img className={`index-donde-img ${scroll > 2400 && 'index-donde-img-animated'}`} src={dondeImg} alt='Donde estamos' />
           </div>
         </div>
       </>
@@ -149,16 +149,18 @@ const IndexPage = ({}) => {
       <Navbar width='451px' />
       <div className='index-container'>
         {renderHeader()}
-        <div className={`text-centered-right ${scroll > 80  && 'text-centered-animated-bottom'}`}>
+        <div className={`text-centered-bottom ${scroll > 80  && 'text-centered-animated-bottom'}`}>
           <TextCentered>En Vinumar somos especialistas en productos derivados de la uva. Combinamos tecnología, agilidad y tradición, con el único objetivo de extraer la máxima calidad de un fruto excepcional: la uva de Castilla-La Mancha.</TextCentered>
         </div>
         {renderProductos()}
         {renderDonde()}
-        <div className={`text-centered-left ${scroll > 3000 && 'text-centered-animated-bottom'}`}>
+        <div className={`text-centered-bottom ${scroll > 3400 && 'text-centered-animated-bottom'}`}>
           <TextCentered width='960px' margin='60px'>Más de 60 años haciendo historia en el mundo de la uva.</TextCentered>
         </div>
       </div>
-        <Button text='SABER MÁS SOBRE VINUMAR' goTo='/nosotros/Valores' style='red-button' width='292px'>SABER MÁS SOBRE VINUMAR</Button>
+        <div className={`index-button-mas ${scroll > 3650 && 'index-button-mas-animated'}`}>
+          <Button text='SABER MÁS SOBRE VINUMAR' goTo='/nosotros/Valores' style='red-button' width='292px'>SABER MÁS SOBRE VINUMAR</Button>
+        </div>
 
       <Footer />
     </main>
