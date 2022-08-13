3c1a40cd574b43be1f9c59145a5cff2eb7b8eea0
Mark <mark.sanjua.sole@gmail.com>
2022-08-13 03:22:37 +0200

    chore change uri

--- src/helpers/navigateTo.js
+++ src/helpers/navigateTo.js
@@ -1,35 +1,46 @@
 export const navigateTo = (pathname, navigate) => {
     if(pathname === '/where-we-are'){
         navigate('/donde-estamos')
+        return true
       }
     if(pathname === '/sustainability'){
     navigate('/sostenibilidad')
+    return true
     }
     if(pathname === '/contact'){
     navigate('/contacto')
+    return true
     }
     if(pathname === '/product/grape-skin-extract'){
     navigate('/producto/extracto-piel-uva')
+    return true
     }
     if(pathname === '/product/wine-extract'){
     navigate('/producto/extracto-vino')
+    return true
     }
     if(pathname === '/product/grape-tannins'){
     navigate('/producto/taninos-uva')
+    return true
     }
     if(pathname === '/product/alcohol'){
     navigate('/producto/alcoholes')
+    return true
     }
     if(pathname === '/product/grape-seed'){
     navigate('/producto/pepita-de-uva')
+    return true
     }
     if(pathname === '/about-us/resources'){
     navigate('/sobre-nosotros/instalaciones')
+    return true
     }
     if(pathname === '/about-us/our-history'){
     navigate('/sobre-nosotros/nuestra-historia')
+    return true
     }
     if(pathname === '/about-us/values'){
     navigate('/sobre-nosotros/valores')
+    return true
     }
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,15 +3,22 @@ import { navigate } from "gatsby"
 import { navigateTo } from "../helpers/navigateTo"
 
 const NotFoundPage = (props) => {
-  navigateTo(props.location.pathname, navigate)
+  const redirection = navigateTo(props.location.pathname, navigate)
 
-  return (
-    <main>
-      <title>Not found</title>
-      <h2>Not Found</h2>
-
-    </main>
-  )
+  if(redirection){
+    return (
+      <>
+      </>
+    )
+  }else {
+    return (
+      <main>
+        <title>Not found</title>
+        <h2>Not Found</h2>
+  
+      </main>
+    )
+  }
 }
 
 export default NotFoundPage
