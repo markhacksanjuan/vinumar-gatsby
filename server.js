c0b10fbf4573e50fa34208081d7e0448d4017cbb
Mark <mark.sanjua.sole@gmail.com>
2022-10-12 16:36:30 +0200

    chore server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'index.html'))
 })
 app.listen(process.env.PORT || 8080)
