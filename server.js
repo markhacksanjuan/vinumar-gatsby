ae87d8dcc26dafe1168a20709681e5963216eea8
Mark <mark.sanjua.sole@gmail.com>
2022-10-12 17:07:30 +0200

    deploy

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 app.listen(process.env.PORT || 8080)
