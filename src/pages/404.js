258dc004ef621527dad8a59c3ff1a44bf6edf4d9
Mark <mark.sanjua.sole@gmail.com>
2022-11-05 06:36:28 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,8 @@ import { navigateTo } from "../helpers/navigateTo"
 
 const NotFoundPage = (props) => {
 
+  return navigate('/')
+
   if(props.location.pathname === '/productos/avialsa/enocianina'){
       return navigate('/producto/extracto-piel-uva')
   }else {
