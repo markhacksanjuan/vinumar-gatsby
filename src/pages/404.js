5f4846050d6dab32ea0da1a6d5148a5a1a95fd4e
Mark <mark.sanjua.sole@gmail.com>
2022-11-05 06:29:08 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,28 +3,13 @@ import { navigate } from "gatsby"
 import { navigateTo } from "../helpers/navigateTo"
 
 const NotFoundPage = (props) => {
-  const redirection = navigateTo(props.location.pathname, navigate)
-
-  // useEffect(() => {
-  //   if(props.location.pathname === '/productos/avialsa/enocianina') {
-  //     navigate('/producto/extracto-piel-uva/')
-      
-  //   }
-  // }, [])
 
   if(props.location.pathname === '/productos/avialsa/enocianina'){
-      navigate('/producto/extracto-piel-uva')
-    return (
-      <>
-      </>
-    )
+      return navigate('/producto/extracto-piel-uva')
   }else {
     return (
       <main>
         <title>Not found</title>
-        {/* <h2>Not Found</h2> */}
-        <p>{props.location.pathname}</p>
-  
       </main>
     )
   }
