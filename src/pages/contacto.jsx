560d0d809390fbfb3f621ff41ab246af50c71683
Mark <mark.sanjua.sole@gmail.com>
2022-11-11 12:17:53 +0100

    chore contact

--- src/pages/contacto.jsx
+++ src/pages/contacto.jsx
@@ -1,4 +1,6 @@
 import React, { useContext, useEffect } from 'react'
+import { navigate } from 'gatsby'
+import { useForm } from 'react-hook-form'
 import Navbar from '../components/navbarIndex/NavbarIndex'
 import Footer from '../components/footer/Footer'
 import Gmap from '../components/gmap/Gmap'
@@ -16,6 +18,8 @@ const ContactoPage = (props) => {
     const { lang } = useContext(LangStateContext)
     const dispatch = useContext(LangDispatchContext)
 
+    const { handleSubmit, register } = useForm({})
+
     useEffect(() =>{
         historyState(window.location.state, dispatch)
     }, [])
@@ -32,24 +36,42 @@ const ContactoPage = (props) => {
     const onClick = e => {
         console.log('clic')
         e.preventDefault()
+        // navigate('/')
+    }
+    const onSubmit = async (data) => {
+        console.log(data)
+        try{
+            const response = await fetch('https://angry-mccarthy.217-160-209-206.plesk.page/contact', {
+                method: 'POST',
+                body: data,
+                mode: 'cors',
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            })
+            console.log(response)
+        }catch(err) {
+            console.error(err)
+        }
+        // navigate('/')
     }
 
     const renderForm = () => {
         return(
             <>
-                <form method="POST" action='https://formspree.io/f/xqkjallg'>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <p>{langText.contact.form.title[lang]}</p>
-                    <input type="text" name='name' placeholder={langText.contact.form.name[lang]} />
-                    <input type="email" name='email' placeholder={langText.contact.form.email[lang]} />
+                    <input {...register('name')} type="text" name='name' placeholder={langText.contact.form.name[lang]} />
+                    <input {...register('email')} type="email" name='email' placeholder={langText.contact.form.email[lang]} />
                     <div>
-                        <textarea name="message" id="mensaje" cols="48" rows="15" placeholder={langText.contact.form.message[lang]}></textarea>
+                        <textarea {...register('message')} name="message" id="mensaje" cols="48" rows="15" placeholder={langText.contact.form.message[lang]}></textarea>
                     </div>
                     <div id='checkbox' >
                         <div className='send-div'>
-                            <input className='input-checkbox' type="checkbox" name='rgpd' />
+                            <input {...register('rgpd')} className='input-checkbox' type="checkbox" name='rgpd' />
                             <label for="rgpd">{langText.contact.form.accept[lang]}</label>
                         </div>
-                        <Button type='submit' style='send-button' onClickButton={onClick}>{langText.contact.form.send[lang]}</Button>
+                        <Button type='submit' style='send-button'>{langText.contact.form.send[lang]}</Button>
                     </div>
                 </form>
             </>
