60ca27d1dc95d683284078eb9f6fbf9c8bca5502
Mark <mark.sanjua.sole@gmail.com>
2022-11-17 18:16:31 +0100

    changed gitignore

---
+++ src/lang/translations/lang_modal.js
@@ -0,0 +1,16 @@
+export const langText_modal = {
+    messageSuccess: {
+        title: {
+            es: '¡Muchas gracias!',
+            en: 'Thank you!'
+        },
+        message: {
+            es: 'Su mensaje ha sido enviado.',
+            en: 'Your message has vent sent.'
+        }
+    },
+    messageError: {
+        es: 'Debes aceptar la Política de Privacidad para enviar el mensaje.',
+        en: 'Privacy Policy must be accepted to send the message'
+    }
+}
