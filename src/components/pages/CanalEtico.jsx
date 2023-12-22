4ec273504bde81c3ddc47ea4e1bad5cb88a5083e
Mark <mark.sanjuan.sole@gmail.com>
2023-12-22 08:03:06 +0100

    changed styles

--- src/components/pages/CanalEtico.jsx
+++ src/components/pages/CanalEtico.jsx
@@ -37,7 +37,7 @@ const CanalEtico = ({
                             <a className='ethic-link' target='_blank' href="https://bfef6082fa25db8b9586.canal.h2c.app/form.html">{langText.ethic.link[lang]}</a>
                             <div className='qr-container'>
                                 <img className='qr-code' src={qrCode} alt="QR Code" />
-                                <iframe allowtransparency="true" style={{backgroundColor: 'transparent'}} name="form-complaint" src="https://bfef6082fa25db8b9586.canal.h2c.app/form.html" frameborder="0" border="0" width="100%" height='100%'></iframe>
+                                <iframe className='iframe' allowtransparency="true" style={{backgroundColor: 'transparent'}} name="form-complaint" src="https://bfef6082fa25db8b9586.canal.h2c.app/form.html" frameborder="0" border="0" width="100%" height='100%'></iframe>
                             </div>
                         </div>
                     </div>
